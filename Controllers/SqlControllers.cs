116b11b75c74938bf63158c4a3e3a952befc8a26
andre <andresmolina@estudiantec.cr>
2023-06-08 15:28:27 -0600

    Commit #10: Se desarrolla la lógica de la instruccion insert que se recibe del frontend

--- Controllers/SqlControllers.cs
+++ Controllers/SqlControllers.cs
@@ -9,8 +9,61 @@ namespace sqlControllers {
     public class sqlController : ControllerBase {
         [HttpPost]
         [Route("insert/{data}")]
-        public IActionResult Register(string data) {
-            Console.WriteLine(data);
+        public IActionResult insertSql(string data) {
+            //INSERT INTO ESTUDIANTE (carne,nombre,apellido) VALUES (2020129522,Andres,Molina)
+            //DELETE FROM ESTUDIANTE
+            //DELETE FROM ESTUDIANTE WHERE nombre=andres 
+
+            //Hace una lista de strings que se separan cuando hay un espacio " "
+            string[] partes_data = data.Split(" ");
+
+            //Selecciona el nombre de la tabla
+            string nombre_tabla = partes_data[2];
+
+            //Selecciona los atributos que se van a agregar quitandole los parentesis
+            //carne,nombre,apellido
+            string atributos = partes_data[3].Substring(1,partes_data[3].Length -2 );
+
+            //Hace una lista de string usando los atributos
+            //[carne,nombre,apellido]
+            string[] lista_atributos = atributos.Split(",");
+
+            //Selecciona la informacion de los atributos quitandole los parentesis
+            //2020129522,Andres,Molina
+            string info_atributos = partes_data[5].Substring(1,partes_data[5].Length -2 );
+
+            //Hace una lista de string usando los atributos
+            //[2020129522,Andres,Molina]
+            string[] lista_info_atributos = info_atributos.Split(",");
+
+            
+            string xmlFilePath = "../Proyecto_III_Datos_II_Servidor/Xmls/Stores/Stores.xml";
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlFilePath);
+
+            for(int i = 0; i < lista_info_atributos.Length; i++){
+                XmlNodeList storeElements = xmlDoc.SelectNodes($"//Store[Nombre_Store='{nombre_tabla}']/Atributos/"+lista_atributos[i]);
+               foreach (XmlElement attributeElement in storeElements)
+            {
+                attributeElement.InnerText = lista_info_atributos[i];
+                Console.WriteLine(lista_info_atributos[i]);
+            }
+            }
+            xmlDoc.Save("../Proyecto_III_Datos_II_Servidor/Xmls/Stores/Stores.xml"); 
+
+        
+            //return Ok(attributes);
+            
+
+
+            Console.WriteLine(lista_atributos[0]);
+            Console.WriteLine(lista_atributos[1]);
+            Console.WriteLine(lista_atributos[2]);
+            Console.WriteLine(lista_info_atributos.Length);
+            Console.WriteLine(lista_info_atributos[0]);
+            Console.WriteLine(lista_info_atributos[1]);
+            Console.WriteLine(lista_info_atributos[2]);
             //Xml_manager.add(data);
             //JSONManager.AddToJSON<User>(newUser, "../tuto-api/DB/Entities/User.json");
             return Ok();
